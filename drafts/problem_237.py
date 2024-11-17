8f12561eadeeaae5c1f88d4df58030b662ecf699
azizanwazir <azizanwazir@gmail.com>
2024-11-17 23:11:39 +0800

    Added custom logging function, default main() function and runtime timing

--- drafts/problem_237.py
+++ drafts/problem_237.py
@@ -4,3 +4,22 @@
 
 # Project Euler Website: https://projecteuler.net/archives
                             
+import time
+from datetime import datetime
+
+# for custom command line logging                            
+def log_msg(status, msg):
+    date_str = datetime.strftime(datetime.now(), "%d %b %Y %H:%M:%S")
+    status = status.upper().ljust(8)
+    print("{} :: {} - {}".format(date_str, status, msg))
+                            
+def main():
+    pass
+                                               
+if __name__ == "__main__":
+    log_msg("info", "Starting processing of Problem 237")
+    start_time = time.time()
+    main()
+    end_time = time.time() - start_time
+    log_msg("info", "Time taken to run main function: {} seconds".format(end_time))
+                            
